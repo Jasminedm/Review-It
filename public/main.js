202bb8c4c6a5c6e51e3dd1f21f5d18ee6de659b6
Jasminedm <mooredjasminedm01@gmail.com>
2022-11-09 20:03:43 -0500

    style additions

--- public/main.js
+++ public/main.js
@@ -50,32 +50,6 @@ Array.from(heart).forEach(function(element) {
   });
 });
 
-// Array.from(thumbDown).forEach(function(element) {
-//   element.addEventListener('click', function(){
-//     const name = this.parentNode.parentNode.childNodes[1].innerText
-//     const msg = this.parentNode.parentNode.childNodes[3].innerText
-//     const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-//     const id = this.parentNode.parentNode.getAttribute('data-objectId')
-//     fetch('messages', {
-//       method: 'put',
-//       headers: {'Content-Type': 'application/json'},
-//       body: JSON.stringify({
-//         'name': name,
-//         'msg': msg,
-//         'thumbUp':thumbUp -2,
-//         'id': id
-//       })
-//     })
-//     .then(response => {
-//       if (response.ok) return response.json()
-//     })
-//     .then(data => {
-//       console.log(data)
-//       window.location.reload(true)
-//     })
-//   });
-// });
-
 Array.from(trash).forEach(function(element) {
       element.addEventListener('click', function(){
         //const name = this.parentNode.parentNode.childNodes[1].innerText
